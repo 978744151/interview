1294507ba4f5ed7a2d1fb3e1848753888abe8568
ct <978744151@qq.com>
2025-03-27 09:32:08 +0800

    login

--- src/components/FloatingButton/index.tsx
+++ src/components/FloatingButton/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { PlusOutlined } from '@ant-design/icons';
 import './index.scss'; import { useNavigate } from 'react-router-dom';
-
+import { getStore } from '@/utils/store.ts'
+import { message } from 'antd';
 interface FloatingButtonProps {
     icon?: React.ReactNode;
     onClick?: () => void;
@@ -16,6 +17,11 @@ const FloatingButton: React.FC<FloatingButtonProps> = ({
     const navigate = useNavigate();
 
     const handleCreatePost = () => {
+        if (!getStore({ name: 'token' })) {
+            message.error('请先登录！');
+            navigate('/login');
+            return;
+        }
         navigate('/blog/create');
     };
     return (
